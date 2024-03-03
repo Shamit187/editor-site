dfd6bbb5c14a410cea625e82ab1b83c6d02a17e5
Shamit187 <shamit187@gmail.com>
2024-03-03 23:52:47 +0600

    Need to have radix-ui man

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const ubuntu = Ubuntu({
   weight: "400",
   style: "normal",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Journa",
-  description: "Site for uploading content for my note site, blog site and gallery site",
+  description:
+    "Site for uploading content for my note site, blog site and gallery site",
   icons: {
     icon: [
       {
         media: "(prefers-color-scheme: dark)",
-        url : "/logo_white.svg",
+        url: "/logo_white.svg",
         href: "/logo_white.svg",
       },
       {
         media: "(prefers-color-scheme: light)",
-        url : "/logo_black.svg",
+        url: "/logo_black.svg",
         href: "/logo_black.svg",
       },
-    ]
-  }
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -34,7 +36,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
